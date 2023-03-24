cc5fd637144f6126eb8f7fd1899bf2833cb69e77
Trizzie Huynh <putmind2822@gmail.com>
2023-03-24 15:11:49 -0700

    added the code for app.post

--- server.js
+++ server.js
@@ -1,20 +1,49 @@
 const express = require("express");
 const path = require("path");
 
+const notes = require("./db/db.json");
+
+const fs = require("fs");
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//retrieving the files from the html
+//GET * from the index throguh html routes
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-//get notes
+//get notes from HTML routes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+//get api/notes from api routes
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+//post /api/notes
+app.post("/api/notes", (req, res) => {
+  let newNote = {
+    title: req.body.title,
+    text: req.body.text,
+  };
+
+  notes.push(newNote);
+  fs.writeFile(
+    path.join(__dirname, "db", "db.json"),
+    JSON.stringify(notes),
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+      res.json(newNote)
+    }
+  );
 });
 
 const PORT = process.env.PORT || 3001;
